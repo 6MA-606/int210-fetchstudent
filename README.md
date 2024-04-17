58a4f4bf014c599c258447bc2c82bed9448c00c7
Sittha Manittayakul <zyxma.6666@hotmail.com>
2024-04-18 00:36:37 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# READ ME!
+
+this is test frontend to intregrate with LAB07
+
+**You must run LAB07 student-api on port 9000**
---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+const API_URL = `http://lvm00000.sit.kmutt.ac.th:9000/api/students`
+
+export default {
+  data: () => ({
+    student: null
+  }),
+
+  created() {
+    // fetch on init
+    this.fetchData()
+  },
+
+  methods: {
+    async fetchData() {
+      const url = `${API_URL}`
+      this.student = await (await fetch(url)).json()
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>Fetch Students from API</h1>
+
+  <p>{{ student }}</p>
+</template>
